d0ca59ec990771eab025143809ab9e6cdc5d7c83
geng <snggeng@gmail.com>
2017-09-09 11:59:25 -0400

    adding color change function

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
 import {  DatePickerIOS,
           StyleSheet,
           Text,
           TextInput,
           Image,
-          View, } from 'react-native';
+          View, } from 'react-native'
+import { CheckBox } from 'react-native-elements'
+
 
 export default class App extends Component {
   constructor(props){
@@ -12,7 +14,10 @@ export default class App extends Component {
     let d = new Date()
     this.state = {
       date: d,
-      title: ''
+      title: '',
+      contact: '',
+      checked: true,
+      repeat: [false, false, false, false, false, false, false]
     }
   }
   dateChange = (e) => {
@@ -20,6 +25,14 @@ export default class App extends Component {
     this.setState({
       date:e
     })
+  }
+
+  handleClick = (i, e) => {
+    let newState = [...this.state.repeat]
+    console.log(i)
+    newState[i] ? newState[i]=false : newState[i]=true
+    console.log(newState)
+    //this.setState({repeat: newState})
   }
   render() {
     return (
@@ -35,25 +48,23 @@ export default class App extends Component {
                         style={styles.datepicker}
                         mode="datetime"
                         onDateChange= {this.dateChange}/>
+        {/* <Text>Repeat</Text> */}
+        <CheckBox title='Repeat' checked={this.state.checked} style={{marginTop: '30%', padding: '5%'}}/>
+        <View style={styles.repeat}>
+
+        { ['S','M','T','W','T','F','S'].map((day, i) => {
+          return (  <Image key={i} style={this.state.repeat[i] ? styles.circleChecked : styles.circle} >
+                      <Text onPress={(e) => {this.handleClick(i, e)}}>{day}</Text>
+                    </Image>)
+        }) }
+        </View>
         <TextInput
             style={{marginTop: "30%", height: 40, textAlign: 'center', borderBottomColor: '#000000', borderBottomWidth: 1,}}
-            placeholder={'Please enter Task Title'}
+            placeholder={'Please enter Contact'}
             placeholderTextColor={'gray'}
-            onChangeText={(text) => this.setState({title: text})}
-            value={this.state.title}
+            onChangeText={(text) => this.setState({contact: text})}
+            value={this.state.contact}
           />
-          <Text>Repeat</Text>
-          <View style={styles.repeat}>
-
-          { ['S','M','T','W','T','F','S'].map((day, i) => {
-            return (  <Image key={i} style={styles.circle}>
-                        <Text>{day}</Text>
-                      </Image>)
-          }) }
-          </View>
-          {/* <View>
-            <Image style={styles.circle} source={{uri: 'http://placehold.it/100x100'}}/>
-          <View/> */}
       </View>
     );
   }
@@ -75,14 +86,24 @@ const styles = StyleSheet.create({
     //width: '500px'
   },
   circle: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    marginTop: '10%',
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    margin: '1%',
     backgroundColor: 'blue',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+ },
+ circleChecked: {
+   width: 40,
+   height: 40,
+   borderRadius: 20,
+   margin: '1%',
+   backgroundColor: 'red',
+   flex: 1,
+   justifyContent: 'center',
+   alignItems: 'center',
  },
  repeat: {
    flexWrap: 'wrap',
