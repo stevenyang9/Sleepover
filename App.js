5a58af7b5dd1cbf2993d4d21ea6a79b9190463bf
sd268 <stephanie.ding@gmail.com>
2017-09-09 10:38:05 -0400

    add firebase

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
+import * as firebase from 'firebase';
+
 import {  DatePickerIOS,
           StyleSheet,
           Text,
           TextInput,
           View, } from 'react-native';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAWBZPThtjYc-K0P6OhVYrKv9yLT2y7XNM",
+  authDomain: "sleepover-pennapps2017.firebaseapp.com",
+  databaseURL: "https://sleepover-pennapps2017.firebaseio.com",
+  storageBucket: "sleepover-pennapps2017.appspot.com",
+  messagingSenderId: "990954020304"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default class App extends Component {
   constructor(props){
